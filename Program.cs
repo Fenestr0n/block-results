536b4c71dd5bd928964f48836ceb116724b414ba
Roman Kozhin <kozhin.rm@gmail.com>
2022-12-14 12:35:57 +0300

    Добавлен метод GetMinThreeSymbols

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Входные данные
+string[] array = { "hello", "2", "world", ":-)" };
+
+
+// Сформировать массив из имеющегося, строки которого <= 3 символа
+string[] GetMinThreeSymbols(string[] array)
+{
+    string[] result = { };
+
+    foreach (string item in array)
+    {
+        if (item.Length <= 3)
+        {
+            result.Append(item);
+        }
+    }
+    return result;
+}
+
+GetMinThreeSymbols(array); // ["2", ":-)"]
